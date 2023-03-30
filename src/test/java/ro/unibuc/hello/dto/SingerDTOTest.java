e838def2b2d660c4a499ee2816f1502806a5e694
mirceap24 <123969630+mirceap24@users.noreply.github.com>
2023-03-30 18:59:44 +0300

    [TEST] Updated SingerDTOTest 100% coverage (#7)

--- src/test/java/ro/unibuc/hello/dto/SingerDTOTest.java
+++ src/test/java/ro/unibuc/hello/dto/SingerDTOTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ro.unibuc.hello.data.SingerEntity;
 
 import java.util.Arrays;
 
@@ -85,4 +86,83 @@ class SingerDTOTest {
     void testToString() {
         Assertions.assertEquals(singerDTOString, singerDTO.toString());
     }
+
+    @Test
+    void test_defaultConstructor() {
+        SingerDTO defaultSingerDTO = new SingerDTO();
+        Assertions.assertNull(defaultSingerDTO.getId());
+        Assertions.assertNull(defaultSingerDTO.getName());
+        Assertions.assertEquals(0, defaultSingerDTO.getAge());
+        Assertions.assertNull(defaultSingerDTO.getGenre());
+        Assertions.assertEquals(0, defaultSingerDTO.getPopularity());
+        Assertions.assertNull(defaultSingerDTO.getHitSongs());
+    }
+
+    @Test
+    void test_constructorWithSingerEntity() {
+        SingerEntity singerEntity = new SingerEntity();
+        singerEntity.setId(id);
+        singerEntity.setName(name);
+        singerEntity.setAge(age);
+        singerEntity.setGenre(genre);
+        singerEntity.setPopularity(popularity);
+        singerEntity.setHitSongs(hitSongs);
+
+        SingerDTO singerDTOFromEntity = new SingerDTO(singerEntity);
+        Assertions.assertEquals(id, singerDTOFromEntity.getId());
+        Assertions.assertEquals(name, singerDTOFromEntity.getName());
+        Assertions.assertEquals(age, singerDTOFromEntity.getAge());
+        Assertions.assertEquals(genre, singerDTOFromEntity.getGenre());
+        Assertions.assertEquals(popularity, singerDTOFromEntity.getPopularity());
+        Assertions.assertArrayEquals(hitSongs, singerDTOFromEntity.getHitSongs());
+    }
+
+    @Test
+    void test_constructorWithNameAgeGenrePopularityHits() {
+        SingerDTO singerDTOWithParams = new SingerDTO(name, age, genre, popularity, hitSongs);
+        Assertions.assertNull(singerDTOWithParams.getId());
+        Assertions.assertEquals(name, singerDTOWithParams.getName());
+        Assertions.assertEquals(age, singerDTOWithParams.getAge());
+        Assertions.assertEquals(genre, singerDTOWithParams.getGenre());
+        Assertions.assertEquals(popularity, singerDTOWithParams.getPopularity());
+        Assertions.assertArrayEquals(hitSongs, singerDTOWithParams.getHitSongs());
+    }
+
+    @Test
+    void setId() {
+        String newId = "123456abcdef7890";
+        singerDTO.setId(newId);
+        Assertions.assertEquals(newId, singerDTO.getId());
+    }
+
+    @Test
+    void setName() {
+        String newName = "Inna";
+        singerDTO.setName(newName);
+        Assertions.assertEquals(newName, singerDTO.getName());
+    }
+
+    @Test
+    void setAge() {
+        int newAge = 40;
+        singerDTO.setAge(newAge);
+        Assertions.assertEquals(newAge, singerDTO.getAge());
+    }
+
+    @Test
+    void setGenre() {
+        String newGenre = "Pop";
+        singerDTO.setGenre(newGenre);
+        Assertions.assertEquals(newGenre, singerDTO.getGenre());
+    }
+
+    @Test
+    void setHitSongs() {
+        String[] newHitSongs = new String[]{"Undeva in Balcani", "Maidanez"};
+        singerDTO.setHitSongs(newHitSongs);
+        Assertions.assertArrayEquals(newHitSongs, singerDTO.getHitSongs());
+    }
 }
+
+
+
